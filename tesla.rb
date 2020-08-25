db91bdce15efbe2974e123dca9742fd113fa6fc8
Jeff Kowalski <jeff.kowalski@gmail.com>
2020-08-25 15:40:14 -0700

    accommodate multiple vehicles per account

--- tesla.rb
+++ tesla.rb
@@ -53,39 +53,39 @@ class Tesla < Thor
                                          client_secret: credentials[:client_secret])
         begin
           tesla_api.login!(account[:password])
-          vehicle = tesla_api.vehicles.first
-          @logger.debug vehicle
-        rescue Faraday::ClientError => e
-          @logger.info "vehicle for account[:username] is unavailable #{e}"
-        else
-          if vehicle.state != 'online'
-            @logger.info "#{vehicle['display_name']} is #{vehicle.state}"
-          else
-            begin
-              charge_state = vehicle.charge_state
-              if charge_state.nil?
-                @logger.warn "#{vehicle['display_name']} cannot be queried"
-              else
-                @logger.info "#{vehicle['display_name']} is #{vehicle['state']}, #{charge_state['charging_state']} " \
-                             "with a SOC of #{charge_state['battery_level']}% " \
-                             "and an estimated range of #{charge_state['est_battery_range']} miles " \
-                             "timestamp #{charge_state['timestamp']}"
+          tesla_api.vehicles.each do |vehicle|
+            @logger.debug vehicle
+            if vehicle.state != 'online'
+              @logger.info "#{vehicle['display_name']} is #{vehicle.state}"
+            else
+              begin
+                charge_state = vehicle.charge_state
+                if charge_state.nil?
+                  @logger.warn "#{vehicle['display_name']} cannot be queried"
+                else
+                  @logger.info "#{vehicle['display_name']} is #{vehicle['state']}, #{charge_state['charging_state']} " \
+                               "with a SOC of #{charge_state['battery_level']}% " \
+                               "and an estimated range of #{charge_state['est_battery_range']} miles " \
+                               "timestamp #{charge_state['timestamp']}"
 
-                tags = { display_name: vehicle['display_name'].tr("'", '_') }
-                timestamp = charge_state['timestamp']
-                data = [{ series: 'est_battery_range', values: { value: charge_state['est_battery_range'].to_f  }, tags: tags, timestamp: timestamp },
-                        { series: 'state',             values: { value: vehicle['state'] },                        tags: tags, timestamp: timestamp }]
-                data.push({ series: 'charging_state',  values: { value: charge_state['charging_state'] },          tags: tags, timestamp: timestamp }) if charge_state['charging_state']
-                influxdb.write_points data unless options[:dry_run]
+                  tags = { display_name: vehicle['display_name'].tr("'", '_') }
+                  timestamp = charge_state['timestamp']
+                  data = [{ series: 'est_battery_range', values: { value: charge_state['est_battery_range'].to_f }, tags: tags, timestamp: timestamp },
+                          { series: 'state',             values: { value: vehicle['state'] },                       tags: tags, timestamp: timestamp }]
+                  data.push({ series: 'charging_state',  values: { value: charge_state['charging_state'] },         tags: tags, timestamp: timestamp }) if charge_state['charging_state']
+                  influxdb.write_points data unless options[:dry_run]
+                end
+              rescue Faraday::ClientError => e
+                @logger.info "#{vehicle['display_name']} is unavailable, #{vehicle.state} #{e}"
               end
-            rescue Faraday::ClientError => e
-              @logger.info "#{vehicle['display_name']} is unavailable, #{vehicle.state} #{e}"
             end
+          rescue Faraday::ClientError => e
+            @logger.info "vehicles for account[:username] are unavailable #{e}"
           end
         end
+      rescue StandardError => e
+        @logger.error e
       end
-    rescue StandardError => e
-      @logger.error e
     end
   end
 end
